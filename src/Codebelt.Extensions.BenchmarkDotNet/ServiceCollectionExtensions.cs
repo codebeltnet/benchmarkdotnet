d9b5d20d73ac211f56703848ec407db393a89b0c
Michael Mortensen <michael@geekle.io>
2025-12-11 22:24:56 +0100

    ✨ update type parameter in AddBenchmarkWorkspace method for clarity

--- src/Codebelt.Extensions.BenchmarkDotNet/ServiceCollectionExtensions.cs
+++ src/Codebelt.Extensions.BenchmarkDotNet/ServiceCollectionExtensions.cs
@@ -21,23 +21,23 @@ namespace Codebelt.Extensions.BenchmarkDotNet
         }
 
         /// <summary>
-        /// Adds a benchmark workspace implementation of type <typeparamref name="TImplementation"/> to the specified <see cref="IServiceCollection"/>
+        /// Adds a benchmark workspace implementation of type <typeparamref name="TWorkspace"/> to the specified <see cref="IServiceCollection"/>
         /// </summary>
-        /// <typeparam name="TImplementation">The type that implements the <see cref="IBenchmarkWorkspace"/> interface.</typeparam>
+        /// <typeparam name="TWorkspace">The type that implements the <see cref="IBenchmarkWorkspace"/> interface.</typeparam>
         /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
         /// <param name="setup">The <see cref="BenchmarkWorkspaceOptions" /> which may be configured.</param>
         /// <returns>The original <see cref="IServiceCollection"/> instance for chaining.</returns>
         /// <remarks>
         /// Validates the <paramref name="services"/> parameter and the provided <paramref name="setup"/> configurator.
-        /// Registers <see cref="IBenchmarkWorkspace"/> as a singleton using <typeparamref name="TImplementation"/>,
+        /// Registers <see cref="IBenchmarkWorkspace"/> as a singleton using <typeparamref name="TWorkspace"/>,
         /// applies the provided configuration, and registers the resolved <see cref="BenchmarkWorkspaceOptions"/> as a singleton.
         /// </remarks>
-        public static IServiceCollection AddBenchmarkWorkspace<TImplementation>(this IServiceCollection services, Action<BenchmarkWorkspaceOptions> setup = null) where TImplementation : class, IBenchmarkWorkspace
+        public static IServiceCollection AddBenchmarkWorkspace<TWorkspace>(this IServiceCollection services, Action<BenchmarkWorkspaceOptions> setup = null) where TWorkspace : class, IBenchmarkWorkspace
         {
             Validator.ThrowIfNull(services);
             Validator.ThrowIfInvalidConfigurator(setup, out var options);
             return services
-                .AddSingleton<IBenchmarkWorkspace, TImplementation>()
+                .AddSingleton<IBenchmarkWorkspace, TWorkspace>()
                 .Configure(setup ?? (_ => {}))
                 .AddSingleton(options);
         }
